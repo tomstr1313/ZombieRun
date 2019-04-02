d86d557d3da5b8270ec1e500d204cabe2a1b63f6
Tom <tompawelec1347@gmail.com>
2019-04-02 14:28:47 -0400

    0.3

---
+++ SuperZombieRunner/Assets/Scripts/WeaponFire.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponFire : MonoBehaviour
+{
+    public GameObject projectile;
+    public float fireDelta = 0.5F;
+
+    private float nextFire = 0.5F;
+    private GameObject newProjectile;
+    private float myTime = 0.0F;
+    public float damage = 100f;
+
+    void FixedUpdate()
+    {
+        myTime = myTime + Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && myTime > nextFire)
+        {
+            nextFire = myTime + fireDelta;
+            newProjectile = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+
+            // create code here that animates the newProjectile
+
+            nextFire = nextFire - myTime;
+            myTime = 0.0F;
+
+            RaycastHit hit;
+            Ray ray = new Ray(transform.position, transform.right);
+            if(Physics.Raycast(ray,out hit, 100f)) {
+                hit.transform.GetComponent<HealthScript>().RemoveHealth(damage);
+            }
+
+
+
+            
+
+
+        }
+    }
+}
+
